2f1b8db44a9366ffa9ea8abfe7c5148475f600b1
Vatsal Gajera <vatsalgajera@gmail.com>
2026-05-30 13:02:27 +0530

    fix: add Terms of Service and Privacy Policy pages

---
+++ client/src/pages/TermsOfService.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TermsOfService() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Terms of Service</h1>
+      <p>
+        Welcome to Travel Plans. By using this platform, you agree to comply
+        with our terms and conditions.
+      </p>
+    </div>
+  );
+}
+
+export default TermsOfService;
