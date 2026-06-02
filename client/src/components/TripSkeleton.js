18ed64eb6d8a8d44b18d58b21bfc76bf04f590a2
Debasmita <dbose272@gmail.com>
2026-06-02 19:47:15 +0530

    feat: add skeleton loader component for trips view

---
+++ client/src/components/TripSkeleton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Skeleton } from '@mui/material';
+
+export default function TripSkeleton() {
+  return <Skeleton variant='rectangular' width={210} height={118} />;
+}
