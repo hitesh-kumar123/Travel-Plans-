a19066e259e239d276074c06dfe03ad8a8dd9637
PariInCode <parisaxena713@gmail.com>
2026-06-11 18:17:53 +0530

    fix done

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,10 +1,14 @@
 // src/pages/About.js
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import "./Home.css"; // reuse same styles
 
 const About = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="wander-page">
       {/* ═══ HEADER (same as Home) ═══ */}
