7e664c5b20bbec8a5bff5c18a3d78b91f16120e8
Akshita Sharma <sakshita229@gmail.com>
2026-07-24 09:13:54 +0530

    fix: remove unused react-icons to pass CI

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import { FaLinkedin, FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-
 const Home = () => {
   return (
     <div className="packgo-page">
